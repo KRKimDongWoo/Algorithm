d57b6ae273279ead4c479fe588576de58eb28b02
KRKimDongWoo <dhsodhao52@naver.com>
2019-05-27 21:35:55 +0900

    finished hw3

---
+++ hw3/c/hw3_c.h
@@ -0,0 +1,165 @@
+#ifndef HW3C_H
+#define HW3C_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+//parameters
+//adj : adjacent list of graph where the smallest element is the 0th element.
+	//in array adj[i], neighbors of the ith vertecies are inserted.
+//cnt : the number of neighbors of the ith vertex.
+//num_v : the number of vertecies.
+//num_e : the number of edges.
+//ans : ans[i] means that ith vertex is included in the ans[i]th strongly connected component.
+	//ans[i] can be non-negative integer.
+	//if there are multiple answers, please find the lexicoraphically smallest sequence.
+
+//This function should return the number of strongly connnected components.
+
+#define min(x, y) ((x < y) ? x : y)
+#define UNVISITED (0)
+#define FALSE (0)
+#define TRUE (1)
+
+void dfs_list(int root, int** adj, int* cnt);
+void dfs_mat(int root, int** adj, int num_v);
+
+static int* ids;
+static int* low;
+static int* onStack;
+
+static int* stack;
+static int top;
+
+static int id;
+
+int find_scc_with_adj_list(int **adj,int *cnt,int num_v,int num_e,int *ans){
+	ids = (int *) calloc(num_v, sizeof(int));
+	low = (int *) calloc(num_v, sizeof(int));
+	onStack = (int *) calloc(num_v, sizeof(int));
+
+	stack = (int *) calloc(num_v, sizeof(int));
+	top = 0;
+
+	id = 1;
+
+	int* low_translate = calloc(num_v, sizeof(int));
+	int scc_num = 0;
+
+	for(int i = 0; i < num_v; i++) {
+		if(!ids[i]) {
+			dfs_list(i, adj, cnt);
+		}
+		low_translate[i] = -1;
+	}
+
+	for(int i = 0; i < num_v; i++) {
+		int original_low = low[i];
+		if(low_translate[original_low - 1] == -1)
+			low_translate[original_low - 1] = scc_num++;
+		ans[i] = low_translate[original_low - 1];
+	}
+
+	free(ids);
+	free(low);
+	free(onStack);
+	free(stack);
+
+	return scc_num;
+}
+
+void dfs_list(int root, int** adj, int* cnt) {
+
+	stack[top++] = root;
+	onStack[root] = TRUE;
+	ids[root] = (low[root] = id++);
+
+	for(int i = 0; i < cnt[root]; i++) {
+		int node = adj[root][i];
+		if(!ids[node]) {
+			dfs_list(node, adj, cnt);
+		}
+		if(onStack[node]) {
+			low[root] = min(low[node], low[root]);
+		}
+	}
+
+	if(ids[root] == low[root]) {
+		for(int node = stack[--top];; node = stack[--top]) {
+			onStack[node] == FALSE;
+			low[node] = ids[root];
+			if(node == root) break;
+		}
+	}
+}
+
+
+
+//adj : num_v x num_v dimension adjacency matrix of the given graph
+
+//the other parameters have same as above function.
+
+int find_scc_with_adj_mat(int **adj,int num_v,int num_e,int *ans){
+
+	ids = (int *) calloc(num_v, sizeof(int));
+	low = (int *) calloc(num_v, sizeof(int));
+	onStack = (int *) calloc(num_v, sizeof(int));
+
+	stack = (int *) calloc(num_v, sizeof(int));
+	top = 0;
+
+	id = 1;
+
+	int* low_translate = calloc(num_v, sizeof(int));
+	int scc_num = 0;
+
+	for(int i = 0; i < num_v; i++) {
+		if(!ids[i]) {
+			dfs_mat(i, adj, num_v);
+		}
+		low_translate[i] = -1;
+	}
+
+	for(int i = 0; i < num_v; i++) {
+		int original_low = low[i];
+		if(low_translate[original_low - 1] == -1)
+			low_translate[original_low - 1] = scc_num++;
+		ans[i] = low_translate[original_low - 1];
+	}
+
+	free(ids);
+	free(low);
+	free(onStack);
+	free(stack);
+
+	return scc_num;
+}
+
+void dfs_mat(int root, int** adj, int num_v) {
+
+	stack[top++] = root;
+	onStack[root] = TRUE;
+	ids[root] = (low[root] = id++);
+
+	for(int node = 0; node < num_v; node++) {
+		if(!adj[root][node]) continue;
+		else {
+			if(!ids[node]) {
+				dfs_mat(node, adj, num_v);
+			}
+			if(onStack[node]) {
+				low[root] = min(low[node], low[root]);
+			}
+		}
+	}
+
+	if(ids[root] == low[root]) {
+		for(int node = stack[--top];; node = stack[--top]) {
+			onStack[node] == FALSE;
+			low[node] = ids[root];
+			if(node == root) break;
+		}
+	}
+}
+
+#endif
---
+++ hw3/main.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "hw3_c.h"
+
+#ifndef FILE_INPUT_ENABLED
+#define STD_INPUT_ENABLED
+#endif
+
+int main(){
+	int num_v;
+	int num_e;
+	
+	int *cnt;
+	int **adj_list;
+	int **adj_mat;
+	
+	clock_t start_list, start_mat, end_list, end_mat;
+
+	int from, dest;
+#if defined(STD_INPUT_ENABLED)
+	printf("Standard input mode\n");
+	scanf("%d %d", &num_v, &num_e);
+#elif defined(FILE_INPUT_ENABLED)
+#ifndef INPUT_FILE
+#define INPUT_FILE ""
+#endif
+	printf("File input mode\n");
+	FILE *fp = fopen(INPUT_FILE, "r");
+	if(fp == NULL) {
+		printf("Invalid input file: %s\n", INPUT_FILE);
+		exit(1);
+	}
+	printf("Testing with %s\n", INPUT_FILE);
+	fscanf(fp, "%d %d", &num_v, &num_e);
+#endif
+
+	adj_list = (int **) calloc(num_v, sizeof(int*));
+	adj_mat = (int **) calloc(num_v, sizeof(int*));
+	cnt = (int*) calloc(num_v, sizeof(int));
+
+	for(int i = 0; i < num_v; i++) {
+		adj_list[i] = NULL;
+		adj_mat[i] = (int *) calloc(num_v, sizeof(int));
+		cnt[i] = 0;
+	}
+
+	for(int i = 0; i < num_e; i++) {
+#if defined(STD_INPUT_ENABLED)
+		scanf("%d %d", &from, &dest);
+#elif defined(FILE_INPUT_ENABLED)
+		fscanf(fp, "%d %d", &from, &dest);
+#endif
+		adj_list[from] = (int *) realloc(adj_list[from], (++cnt[from]) * sizeof(int));
+		adj_list[from][cnt[from] - 1] = dest;
+
+		adj_mat[from][dest] = 1;
+	}
+
+	int* ans_list = (int*) calloc(num_v, sizeof(int));
+	int* ans_mat = (int*) calloc(num_v, sizeof(int));
+	
+	start_list = clock();
+	find_scc_with_adj_list(adj_list, cnt, num_v, num_e, ans_list);
+	end_list = clock();	
+	
+	start_mat = clock();
+	find_scc_with_adj_mat(adj_mat, num_v, num_e, ans_mat);
+	end_mat = clock();
+
+
+
+#if defined(STD_INPUT_ENABLED)
+	printf("Result on list\n");
+	for(int i = 0; i < num_v; i++)
+		printf("%d ", ans_list[i]);
+	printf("\nResult on matrix\n");
+	for(int i = 0; i < num_v; i++)
+		printf("%d ", ans_mat[i]);
+	printf("\n");
+#elif defined(FILE_INPUT_ENABLED)
+#ifndef OUTPUT_FILE
+#define OUTPUT_FILE ""
+#endif
+	fp = fopen(OUTPUT_FILE, "r");
+	int* ans = (int*) calloc(num_v, sizeof(int));
+	int err_flag = 0;
+
+	printf("Checking list...\n");
+	for(int i = 0; i < num_v; i++) {
+		fscanf(fp, "%d", &ans[i]);
+	}
+	for(int i = 0; i < num_v; i++){
+		if(ans[i] != ans_list[i]) {
+			err_flag = 1;
+			printf("Error on list.\n");
+			printf("On line %d, expected: %d, got: %d\n", i, ans[i], ans_list[i]);
+			break;
+		}
+	}
+
+	if(!err_flag) printf("Successful!\n");
+	err_flag = 0;	
+	
+	printf("Checking matrix...\n");
+	for(int i = 0; i < num_v; i++){
+		if(ans[i] != ans_mat[i]) {
+			err_flag = 1;
+			printf("Error on mat\n");
+			printf("On line %d, expected: %d, got: %d\n", i, ans[i], ans_list[i]);
+			break;
+		}
+	}
+
+	if(!err_flag) printf("Successful!\n");
+#endif
+
+	printf("list takes %0.3fms\n", (float)(end_list - start_list)/(CLOCKS_PER_SEC / 1000));
+	printf("mat takes %0.3fms\n", (float)(end_mat - start_mat)/(CLOCKS_PER_SEC / 1000));
+	return 0;
+}
